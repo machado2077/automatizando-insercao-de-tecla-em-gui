82ddc47d06e8558fbdd6c0287458d82b70a43f77
Matheus <machadom@outlook.com.br>
2021-06-13 22:39:00 -0300

    gerado tempos aleatorios para o intervalo de cada evento de tecla

--- src/keyboard_event_response.py
+++ src/keyboard_event_response.py
@@ -1,4 +1,5 @@
 from time import sleep
+from random import uniform
 from pynput.keyboard import Controller
 from .keyboard_event_mapper import keyboard_event_mapper as key_event
 from .abstract_classes.abstract_keyboard_event_response import AbstractKeyboardEventResponse
@@ -10,7 +11,14 @@ class PynputKeyboardEventResponse(AbstractKeyboardEventResponse):
     def __init__(self) -> None:
         super().__init__()
 
+    def __randomize_sleep_time(self):
+        inferior_limit = 0.7
+        upper_limit = 0.9
+        real_num = uniform(inferior_limit, upper_limit)
+        return float(f'{real_num:.3}')
+
     def _start_key_press(self) -> bool:
         keyboard_controller.tap(key_event.initialize)
-        sleep(0.8) #O sleep precisa estar depois do .tap(key)
+        time = self.__randomize_sleep_time()
+        sleep(time) #O sleep precisa estar depois do .tap(key)
         return True
